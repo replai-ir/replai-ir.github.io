d822b59d604c7a16f5b8124cf3086ff926962aa6
MKhodagholi <mdkhodagholi@gmail.com>
2023-01-09 00:05:23 +0330

    4. add ImageTextReplai & add styles (margins)

--- components/Layout/Nav.jsx
+++ components/Layout/Nav.jsx
@@ -1,14 +1,13 @@
 import Image from "next/image";
 import replaiLogo from "../../public/assets/svg/replai_logo.svg";
+import replaiTextLogo from "../../public/assets/svg/replai_text-Logo.svg";
 
 export default function Nav() {
   return (
-    <nav className="mt-[1.875rem] ml-[2rem] list-none md:ml-[6.25rem]">
+    <nav className="mt-[1.875rem] mr-[2rem] md:mr-[8rem] list-none">
       <li className="flex items-center">
         <Image src={replaiLogo} alt="replai-logo" />
-        <h2 className="ml-2.5 font-Impact text-slightyBlack">
-          Repl<span className="text-blueColor font-Impact">ai</span>
-        </h2>
+        <Image src={replaiTextLogo} alt="replai-text-logo" className="mr-4" />
       </li>
     </nav>
   );
