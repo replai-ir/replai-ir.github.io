b27c30fdb5209fd4166cba90aa1589291695c8f7
MKhodagholi <mdkhodagholi@gmail.com>
2023-01-09 00:04:31 +0330

    3. add font-helveticNeue for footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="h-[70px] bg-grayColor flex items-center justify-center md:h-[100px]">
-      <Link href="https://roshan-ai.ir" className="text-lg">
+      <Link href="https://roshan-ai.ir" className="text-lg font-helveticNeue">
         Roshan
       </Link>
     </footer>
