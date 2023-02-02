0ab9638e9b5df9ce0b4798f00bfd2439e2efac23
MKhodagholi <mdkhodagholi@gmail.com>
2023-02-02 11:18:07 +0330

    13. add cursor pointer into Navbar

--- components/Layout/Nav.jsx
+++ components/Layout/Nav.jsx
@@ -1,13 +1,26 @@
 import Image from "next/image";
 import replaiLogo from "../../public/assets/svg/replai_logo.svg";
 import replaiTextLogo from "../../public/assets/svg/replai_text-logo.svg";
+import Link from "next/link";
 
 export default function Nav() {
   return (
     <nav className="mt-[1.875rem] mar-right list-none">
       <li className="flex items-center">
-        <Image src={replaiLogo} alt="replai-logo" />
-        <Image src={replaiTextLogo} alt="replai-text-logo" className="mr-4" />
+        <Link href="/">
+          <Image
+            src={replaiLogo}
+            alt="replai-logo"
+            className="cursor-pointer"
+          />
+        </Link>
+        <Link href="/">
+          <Image
+            src={replaiTextLogo}
+            alt="replai-text-logo"
+            className="mr-4 cursor-pointer"
+          />
+        </Link>
       </li>
     </nav>
   );
